c0831bebebe4207c00870bc740d9289d81b7a84e
Ty Ham <master@surewest.net>
2013-03-09 10:18:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ee268b1b6a69e0fc1e187e32df011c1df53d931c7e6ac8f67c4fc3df3553d7f33154af5e98b5bc3afe8e18978b8ebd7df02b00185a1ab1f9024057780532980d'
